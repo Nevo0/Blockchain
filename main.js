736bae7cb4f7048be6ffc09230579bd1db9201de
Nevo0 <Piecyk.rafal@gmail.com>
2019-02-21 21:43:10 +0100

    Block

--- main.js
+++ main.js
@@ -12,12 +12,39 @@ class Block {
 
   //
 
-  calculateHash = () => {
+  calculateHash() {
     return SHA256(
       this.index +
         this.previousHash +
         this.timesTamp +
         JSON.stringify(this.data)
     ).toString();
-  };
+  }
 }
+
+class Blockchain {
+  constructor() {
+    this.chain = [this.createGenesisBlock()];
+  }
+
+  createGenesisBlock() {
+    return new Block(0, "01/01/2019", "Benesis Block", "0");
+  }
+
+  getLastBlock() {
+    return this.chain[this.chain.length - 1];
+  }
+
+  addBlock(newBlock) {
+    newBlock.previousHash = this.getLastBlock().hash;
+    newBlock.hash = newBlock.calculateHash();
+    this.chain.push(newBlock);
+  }
+}
+
+let myCoin = new Blockchain();
+myCoin.addBlock(new Block(1, "02/01/2019", { amount: 4 }));
+myCoin.addBlock(new Block(2, "12/01/2019", { amount: 4 }));
+myCoin.addBlock(new Block(3, "014/01/2019", { amount: 4 }));
+
+console.log(JSON.stringify(myCoin, null, 4));
